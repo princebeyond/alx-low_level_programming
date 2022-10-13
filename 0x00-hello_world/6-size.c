5afc4de419a5a21980dff616f1403a188677eef5
princebeyond <amoodeyond222@gmail.com>
2022-10-13 10:27:06 -0700

    life

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	fprintf("size of a char: %i bytes\n", sizeof(char));
-	fprintf("size of an int; %i bytes\n", sizeof(int));
-	fprintf("size of a long int: %i bytes\n", sizeof(long int));
-	fprintf("size of a long long int %i bytes\n", sizeof(long long int));
-	fprintf("sife of a float %i bytes\n", sizeof(float));
+	printf("size of a char: %i bytes\n", sizeof(char));
+	printf("size of an int; %i bytes\n", sizeof(int));
+	printf("size of a long int: %i bytes\n", sizeof(long int));
+	printf("size of a long long int %i bytes\n", sizeof(long long int));
+	printf("size of a float %i bytes\n", sizeof(float));
 	return (0);
 }
