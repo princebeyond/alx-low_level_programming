3aa45a29e3d6eea759eeb87e32416eb65c5af82b
princebeyond <amoodeyond222@gmail.com>
2022-10-13 08:36:12 -0700

    life

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry
+ * Return: Always
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
