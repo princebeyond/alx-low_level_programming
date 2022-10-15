e3f0496e86cf0300b59d06770fa3b8b44b5ebf17
princebeyond <amoodeyond222@gmail.com>
2022-10-15 03:15:47 -0700

    life

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry
- *
- * Alway: Return 0
+ * Always: Return (0)
  */
+
 int main(void)
 {
 	char i;
-
-	printf("Lower case characters:\n");
 	for (i = 'a'; i <= 'z'; i++)
 		printf("%c", i);
 	return (0);
