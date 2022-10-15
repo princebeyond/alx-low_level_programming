3f533787e5daf6f3e3bfa7c66c183403f84afcb9
princebeyond <amoodeyond222@gmail.com>
2022-10-15 03:36:50 -0700

    life

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return 0
+ */
+int main(void)
+{
+	char i;
+
+	for(i = 'A'; i <= 'Z'; i++)
+		printf("%c", i);
+
+	for(i = 'a'; i <= 'z'; i++)
+		printf("%c", i);
+	return (0);
+}
