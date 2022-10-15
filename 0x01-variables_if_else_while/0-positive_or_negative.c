3f285d32f1fc6e59e2a0c9cc12d45eb930686f30
princebeyond <amoodeyond222@gmail.com>
2022-10-15 04:23:00 -0700

    life

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
- */
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	{
-		printf("%d is negative\n", n);
-	} else if (n=<=0);
-	{
-		printf("%d is zero\n", n);
-	} else if   (n==0);
-	{
+
+	if (n > 0)
 		printf("%d is positive\n", n);
-	}
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Entry
- * Return 0
+ * Return (0)
  */
+
 int main(void)
 {
 	char i;
-
-	for(i = 'A'; i <= 'Z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		printf("%c", i);
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
 		printf("%c", i);
 	return (0);
 }
