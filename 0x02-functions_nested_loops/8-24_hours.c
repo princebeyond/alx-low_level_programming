5e5ea1056a1c4dc6239482b64c3a43e882b5b368
princebeyond <amoodeyond222@gmail.com>
2022-10-19 19:25:33 -0700

    life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+	for (hours = 0; hour <= 23; hour++)
+	{
+		for (hours = 0; hour <= 23; hour++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
