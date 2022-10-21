c4877d17da975a6414504d95d2cbb5e44c60cffe
princebeyond <amoodeyond222@gmail.com>
2022-10-21 06:28:06 -0700

    life

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - from 0 to 9
+ *
+ * Return: Answer
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num >= '0'; num <= '9'; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar(0);
+	}
+	_putchar('\n')
+}
