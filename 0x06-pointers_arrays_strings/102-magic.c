174dbcce2630654ff5c2ff3a4a4c0f0ce096be80
princebeyond <amoodeyond222@gmail.com>
2022-10-26 20:55:10 -0700

    life

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 98.
+ *
+ *Return: 0.
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	p = &a[2];
+	*p = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
