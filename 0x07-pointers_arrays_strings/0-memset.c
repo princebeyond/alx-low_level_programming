940acd9f9448838670e9b71345a59dd77c6d8189
princebeyond <amoodeyond222@gmail.com>
2022-11-01 06:26:15 -0700

    life

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,16 @@
 
 /**
  * *_memset - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * @b: the address of memory to print
+ * @s: the size of the memory to print
+ * @n: qwerty
  *
  * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
