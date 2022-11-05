bd8fae826dfaa485a3eb608b8b4f4cd04c69d14d
princebeyond <amoodeyond222@gmail.com>
2022-11-04 17:26:54 -0700

    life

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry - point
+ * @argc: qwerty
+ * @argv: qwerty
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf(Total = %d\n", mul);
+	}
+	return (0);
+}
