1c5ac4331e0e130ba6e46d5ccceb89786c37c404
princebeyond <amoodeyond222@gmail.com>
2023-09-07 04:58:28 -0700

    life

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of a char %2d bytes\n", sizeof(char));
+	printf("size of an int %2d bytes\n", sizeof(int));
+	printf("size of a long int %2d bytes\n", sizeof(long int));
+	printf("size of a long long int %2d bytes\n", sizeof(long long int));
+	printf("size of a float %2d bytes\n", sizeof(float));
+	return (0);
+}
