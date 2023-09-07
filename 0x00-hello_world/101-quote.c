f93409dc7a8c9a2f01431bffd537581f2825f553
princebeyond <amoodeyond222@gmail.com>
2023-09-07 05:55:27 -0700

    life

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	FILE *file = stdout;
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", file);
+	return (1);
+}
