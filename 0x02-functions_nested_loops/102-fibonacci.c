f1ad47f9a2314897b353c507f3eae00c3ba41620
princebeyond <amoodeyond222@gmail.com>
2023-09-13 06:23:09 -0700

    k

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
+	int i;
 	int f = 1, s = 2;
 
 	printf("%d, %d, ", f, s);
 
-	int i;
-
 	for (i = 3; i <= 50; i++)
 	{
 		int nxt = f + s;
+
 		printf("%d, ", nxt);
 
 		f = s;
