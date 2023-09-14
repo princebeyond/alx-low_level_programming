350886ca75f0893914bb47572fce1788ea1a5756
princebeyond <amoodeyond222@gmail.com>
2023-09-14 12:37:15 -0700

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - duhhhhhh
+ *
+ * Return: anything u want 1 or 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
