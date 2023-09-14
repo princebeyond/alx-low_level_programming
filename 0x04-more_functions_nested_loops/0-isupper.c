504597170878e02f3909aa008cad7c04e9066fd2
princebeyond <amoodeyond222@gmail.com>
2023-09-14 04:12:13 -0700

    l

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ * @c: asdf
+ *
+ * Return: success 1 or 0.
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
