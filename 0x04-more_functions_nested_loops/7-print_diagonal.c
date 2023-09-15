25c7c62eb48a9e315cac1ad7b0aa83951bc1504e
princebeyond <amoodeyond222@gmail.com>
2023-09-15 06:34:38 -0700

    f

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - jjsjsjs
+ */
+void print_diagonal(int n)
+{
+	if ( n <= 0)
+	{
+		_putchar('\n');
+
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
