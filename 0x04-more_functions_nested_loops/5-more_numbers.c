e4dfdcabe10f8ee10909a4176b7928a83db7584c
princebeyond <amoodeyond222@gmail.com>
2023-09-15 03:32:55 -0700

    h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - lifee
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - ahh d
+ * @size: egg v
+ *
+ * Return: 1 or 0.
+ */
+void print_square(int size)
+{
+	int i;
+
+	if (size <= 0)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
