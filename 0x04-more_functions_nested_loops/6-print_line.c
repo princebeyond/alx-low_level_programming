91009314e13529b93cd64fbbe0ffd00e87e58b3c
princebeyond <amoodeyond222@gmail.com>
2023-09-15 05:50:42 -0700

    n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,18 @@
  */
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
 	{
-		 if (n <= 0)
-		 {
-			 _putchar('\n');
-		 }
-		 _putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
