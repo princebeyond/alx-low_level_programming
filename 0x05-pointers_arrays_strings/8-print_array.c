09859f061b7f7ae4f910aea51a0db15df5479ffa
princebeyond <amoodeyond222@gmail.com>
2023-09-19 03:32:45 -0700

    l

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - qqqawerty
+ * @n: hghhg
+ * @a: hddhdh
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d", a[0]);
+
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d",a[i]);
+	}
+	printf("\n");
+}
