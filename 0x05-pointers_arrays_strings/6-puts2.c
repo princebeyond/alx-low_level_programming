81aecc7aecea1626774c2dbf935890dfc0fcb06e
princebeyond <amoodeyond222@gmail.com>
2023-09-18 21:53:01 -0700

    l

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - qu wrt
+ * @str: ghhj
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
