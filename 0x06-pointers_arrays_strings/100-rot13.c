51dcacc3ba10d9be24dff610ca023b4a05d149b2
princebeyond <amoodeyond222@gmail.com>
2023-09-21 03:31:52 -0700

    ll

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,18 +6,19 @@
  */
 char *rot13(char *src)
 {
-    int i = 0;
-    while (src[i] != '\0')
-    {
-        if ((src[i] >= 'a' && src[i] <= 'z') || (src[i] >= 'A' && src[i] <= 'Z'))
-        {
-            if ((src[i] >= 'a' && src[i] <= 'm') || (src[i] >= 'A' && src[i] <= 'M'))
-                src[i] += 13;
-            else
-                src[i] -= 13;
-        }
-        i++;
-    }
-    return src;
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+	if ((src[i] >= 'a' && src[i] <= 'z') || (src[i] >= 'A' && src[i] <= 'Z'))
+	{
+		if ((src[i] >= 'a' && src[i] <= 'm') || (src[i] >= 'A' && src[i] <= 'M'))
+		src[i] += 13;
+	else
+		src[i] -= 13;
+	}
+	i++;
+	}
+	return (src);
 }
 
