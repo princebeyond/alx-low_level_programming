9771eeff5345599974ebbfaa33d39f249fbc3c8c
princebeyond <amoodeyond222@gmail.com>
2023-09-21 22:56:44 -0700

    l

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,50 @@
+#include "main.h"
+/**
+ * infinite_add - ll
+ * @n1: hjk
+ * @n2: uihu
+ * @r: hjkjj
+ * @size_r: hjsjjs
+ * Return: success
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0, j = 0, carry = 0;
-    int digit1, digit2, sum;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-    while (n1[i] != '\0')
-        i++;
-
-    while (n2[j] != '\0')
-        j++;
-
-    if (i > size_r || j > size_r)
-        return (0);
-
-    r[i + 1] = '\0';
-
-    i--, j--;
-
-    while (i >= 0 || j >= 0 || carry)
-    {
-        digit1 = (i >= 0) ? n1[i] - '0' : 0;
-        digit2 = (j >= 0) ? n2[j] - '0' : 0;
-
-        sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        r[i + 1] = (sum % 10) + '0';
-
-        i--, j--;
-    }
-
-    return (r);
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+	if (i < 0)
+		val1 = 0;
+	else
+		val1 = *(n1 + i) - '0';
+	if (j < 0)
+		val2 = 0;
+	else
+		val2 = *(n2 + j) - '0';
+	temp_tot = val1 + val2 + overflow;
+	if (temp_tot >= 10)
+		overflow = 1;
+	else
+		overflow = 0;
+	if (digits >= (size_r - 1))
+		return (0);
+	*(r + digits) = (temp_tot % 10) + '0';
+	digits++;
+	j--;
+	i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
-
