9496f9f54e3c51c0364d1f8e224d2f3d1f7f53bd
princebeyond <amoodeyond222@gmail.com>
2023-10-03 06:32:47 -0700

    functionthatcreatesanarrayofchars,andinitializesitwithaspecificchar

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,11 +19,11 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
-	array[size - 1] = '\0';
+	array[size] = '\0';
 
 	return (array);
 }
