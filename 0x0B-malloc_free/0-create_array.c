ec0dbdf2e5c56aeaf61492889c6bfdd4f8359016
princebeyond <amoodeyond222@gmail.com>
2023-10-03 05:42:26 -0700

    functionthatcreatesanarrayofchars,andinitializesitwithaspecificchar

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_array - creating array
+ * @size: size 
+ * @c: char
+ * Return: 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	array = (char *)malloc (sizeof(char) * (size + 1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	array[size] = '\0';
+
+	return (array);
+}
