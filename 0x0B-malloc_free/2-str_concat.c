ba75eb78e56594f3b7c634642d8e29e335c2a8e2
princebeyond <amoodeyond222@gmail.com>
2023-10-04 05:58:41 -0700

    functionthatconcatenatestwostrings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,15 @@ char *str_concat(char *s1, char *s2)
 	size_t tl = len + len2 + 1;
 	char *tt;
 
+
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
 	tt = (char *)malloc(tl);
 
 	if (tt == NULL)
