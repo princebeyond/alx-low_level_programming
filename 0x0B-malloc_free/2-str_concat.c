1ecc378bcf4fef405ca90a7f887a759135ea9438
princebeyond <amoodeyond222@gmail.com>
2023-10-04 02:34:43 -0700

    functionthatconcatenatestwostrings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - concentrate string
+ * @s1: first string
+ * @s2: sec string
+ * Return: success or null.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len = strlen(s1);
+	size_t len2 = strlen(s2);
+	size_t tl = len + len2 + 1;
+	char *tt;
+
+	tt = (char *)malloc(tl);
+
+	if (tt == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(tt, s1);
+
+	strcat(tt, s2);
+
+	return (tt);
+}
