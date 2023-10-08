bb4938201379a18ea9ddcba5bb543e7946cbdf1e
princebeyond <amoodeyond222@gmail.com>
2023-10-08 10:32:45 -0700

    l

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if ((nmemb == 0 || size == 0))
 		return (NULL);
 	total_size = nmemb * size;
-	res = malloc((total_size) + 1);
+	res = malloc(sizeof(int) * total_size);
 	if (res != NULL)
 	{
 		memset(res, 0, sizeof(total_size));
