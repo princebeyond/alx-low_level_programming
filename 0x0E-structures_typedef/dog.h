0217c93c01e533cb0c7e59d61f752c3b457e7267
princebeyond <amoodeyond222@gmail.com>
2023-10-10 04:20:30 -0700

    ll

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 /**
  * struct dog - type structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner
+ *
  */
 struct dog
 {
@@ -9,8 +16,6 @@ struct dog
 	char *owner;
 };
 
-#ifndef DOG_H
-#define DOG_H
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
