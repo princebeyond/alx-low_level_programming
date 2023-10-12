5a6049495b99eaffc36a770432d5ec255d250d1f
princebeyond <amoodeyond222@gmail.com>
2023-10-12 13:26:56 -0700

    test5

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,34 +11,35 @@ void print_all(const char *const format, ...)
 	int i = 0;
 
 	va_start(args, format);
+
 	if (format)
 	{
 
-	while (format[i])
-	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-			printf("%s%c", separator, va_arg(args, int));
-			break;
-			case 'i':
-			printf("%s%d", separator, va_arg(args, int));
-			break;
-			case 'f':
-			printf("%s%f", separator, va_arg(args, double));
-			break;
-			case 's':
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(nill)";
-			printf("%s%s", separator, str);
-			break;
-			default:
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nill)";
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ", ";
 			i++;
-			continue;
-		}
-		separator = ", ";
-		i++;
 	}
 	}
 
