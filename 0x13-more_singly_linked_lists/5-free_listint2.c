66d320fc237554bf35bb422c21ef91a2e562780c
princebeyond <amoodeyond222@gmail.com>
2023-10-24 02:45:20 -0700

    test3

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-	listint_t *next;
+	listint_t *temp;
 
-	current = *head;
 
-	while (current != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
 	*head = NULL;
