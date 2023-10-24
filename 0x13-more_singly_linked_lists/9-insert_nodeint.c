cb09c54f51738f2611ddf3e5feedd4251b9e7471
princebeyond <amoodeyond222@gmail.com>
2023-10-24 01:03:24 -0700

    test2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,18 +12,37 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current = *head;
+	listint_t *previous = NULL;
+	unsigned int count = 0;
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
-
 		return (new_node);
 	}
+
+	while (current != NULL && count < idx)
+	{
+		previous = current;
+		current = current->next;
+		count++;
+	}
+
+	if (count == idx)
+	{
+		previous->next = new_node;
+		new_node->next = current;
+		return (new_node);
+	}
+
+	free(new_node);
 	return (NULL);
 }
-
