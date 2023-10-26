f51a2a62eb378786a9d48a43daf95a77ad88f90d
princebeyond <amoodeyond222@gmail.com>
2023-10-26 04:55:14 -0700

    test1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * get_bit - getting bit
+ * set_bit - setting bit
  * @n: pointer
  * @index: idx
  *
  * Return: success
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	mask = 1UL << index;
-	return ((n & mask) ? 1 : 0);
+	*n = *n | (1UL << index);
+	return (1);
 }
