4bd9dbaf3548ccd3453295cebe46190e110c32ad
princebeyond <amoodeyond222@gmail.com>
2023-10-26 04:10:59 -0700

    test1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - print
+ * @n: poiner
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
