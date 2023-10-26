2c2250241978056df404076ba57ee89a97481865
princebeyond <amoodeyond222@gmail.com>
2023-10-26 04:41:13 -0700

    test1

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - geting bit
+ * @n: pointer
+ * @index: idx
+ *
+ * Return: success
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	mask = 1UL << index;
+	return ((n & mask) ? 1 : 0);
+}
